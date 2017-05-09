02399b7267451ef0864cd0307fab117145be1855
Josh <Josh@Amanda-Actons-MacBook-Pro.local>
2017-05-09 15:23:57 -0600

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json());
+
+let port = 5000;
+
+app.listen(port, function(){
+  console.log('Cthulhu is docked at port ', port)
+});
+
+let messages = ['hello', 'goodbye', 'go away'];
+
+app.get('/messages', function(req, res, next){
+
+  res.status(200).json({ messages: messages })
+
+})
+
+app.post('/messages', function(req, res, next){
+  messages.unshift({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+})
